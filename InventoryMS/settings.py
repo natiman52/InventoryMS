fd68a9b482bee6b21873daef0ba69a7188eecad0
MyICTProject <natnaelyazchew135210@gmail.com>
2025-03-08 23:17:24 +0300

    fixed static

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -135,7 +135,7 @@ LOGOUT_URL = 'logout'
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+STATIC_ROOT = os.path.join(BASE_DIR,"staticroot")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
