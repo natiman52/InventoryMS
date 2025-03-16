be001ffd405130eed734fc37c78745d8883ea5cd
MyICTProject <natnaelyazchew135210@gmail.com>
2025-03-16 18:54:36 +0300

    add oversizes

--- bills/models.py
+++ bills/models.py
@@ -4,9 +4,11 @@ from accounts.models import Supplier
 from accounts.models import MyUser
 from django.utils import timezone
 thickness_type = ((0.5,"0.5mm"),(0.6,"0.6mm"),(0.7,"0.7mm"),(0.8,"0.8mm"),
-                 (0.9,"0.9mm"),(0.91,"0.9mm(oversize)"),(1.0,"1.0mm"),(1.1,"1.1mm"),
-                 (1.4,"1.4mm"),(1.8,"1.8mm"),
-                  (2.5,"2.5mm"),(3.0,"3.0mm"))
+                 (0.9,"0.9mm"),(1.4,"1.4mm"),(1.8,"1.8mm"),(2.5,"2.5mm"),
+                 (1.0,"1.0mm"),(1.1,"1.1mm"),(1.2,"1.2mm"),(3.0,"3.0mm"),
+                 (0.91,"0.9mm(oversize)"),(1.11,"1.1(oversize)mm"),(1.21,"1.2mm(oversize)"),
+                 (1.01,"1.0mm(oversize)"),(1.81,"1.8(oversize)mm"),(2.51,"2.5(oversize)mm"),
+                 (1.41,"1.4mm(oversize)"))
 class Thickness(models.Model):
     name = models.FloatField(max_length=250,unique=True,choices=thickness_type)
     added =models.IntegerField()
