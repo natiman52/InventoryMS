16adcf22d77a3607e12702048b652ed25087c31b
MyICTProject <natnaelyazchew135210@gmail.com>
2025-05-27 03:57:48 +0300

    changed cron schedule again

--- InventoryMS/settings.py
+++ InventoryMS/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
 }
 
 CRONJOBS = [
-    ('3 * * *', 'InventoryMS.cron.my_backup')
+    ('0 3 * * *', 'InventoryMS.cron.my_backup')
 ]
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': BASE_DIR / "backups"}
