7fd59d37ea10b0bbcb7bb032bc43804ddd763f66
MyICTProject <natnaelyazchew135210@gmail.com>
2025-06-08 11:10:18 +0300

    show total in audit prep

--- invoice/calc.py
+++ invoice/calc.py
@@ -1,11 +1,44 @@
 from django.db.models import Sum
 from bills.models import Bill,bill_type
+
 def get_count_of_clients(item):
     unique_count = []
     for i in item:
         if(not i.client in unique_count):
             unique_count.append(i.client)  
     return unique_count
+
+def get_count_of_clients_debt(item):
+    unique_count = []
+    for i in item:
+        if(len(unique_count) > 0):
+            count = 0
+            test =False
+            for d in unique_count:
+                if not d['name'] == i.client.name:
+                    count += 1
+                if count == len(unique_count):
+                    test =True
+            if(test):
+                unique_count.append({'name':i.client.name,'debt':get_count_of_each_client_debt(item,i.client)})
+        else:
+            unique_count.append({'name':i.client.name,'debt':get_count_of_each_client_debt(item,i.client)})
+    return unique_count
+def get_count_of_each_client_debt(items,client):
+    data = 0
+    cancel = False
+    for i in items:
+        if(cancel):
+            return None
+        if(i.client == client):
+            if(not i.invoice_set.exists()):
+                cancel = True
+            elif(i.invoice_set.all()[0].quantity != 0 ):
+                data += i.invoice_set.all()[0].quantity * i.invoice_set.all()[0].unit_price
+            else:
+                data += 1 * i.invoice_set.all()[0].unit_price
+    return data     
+
 def get_single_client_debt(invoices,client):
     data = 0
     for i in invoices:
